173927fc061a03c322863449ffd6d7e0c4b7819b
maksim <maksimkasimov180@gmail.com>
2026-03-08 22:17:21 +0300

    MVP

--- server/main.go
+++ server/main.go
@@ -6,7 +6,7 @@ import (
 	"image"
 	"image/draw"
 	_ "image/jpeg"
-	_ "image/png"
+	"image/png"
 	"net"
 	"os"
 
@@ -30,7 +30,13 @@ func main() {
 	}
 	fmt.Println("Сервер слушает на порту 8080")
 
-	go CuttingAndDistribution(jobs)
+	totalTasks, rgbaImg := CuttingAndDistribution(jobs)
+	if rgbaImg == nil {
+		fmt.Println("Критическая ошибка: невозможно подготовить изображение.")
+		return
+	}
+
+	go ImageCollector(results, rgbaImg, totalTasks)
 
 	for {
 		conn, err := ln.Accept()
@@ -43,16 +49,29 @@ func main() {
 	}
 }
 
-func handleWorker(conn net.Conn, jobs, retry, results chan protocol.Task) {
+func handleWorker(conn net.Conn, jobs, retry chan protocol.Task, results chan protocol.Result) {
 	defer conn.Close()
 	encoder := json.NewEncoder(conn)
+	decoder := json.NewDecoder(conn)
 	for {
 		task := getTask(jobs, retry)
 		err := encoder.Encode(task)
 		if err != nil {
+			fmt.Println("Воркер отвалился при отправке. Возвращаем задачу...")
 			retry <- task
+			return
 		}
+
+		var res protocol.Result
+		err = decoder.Decode(&res)
+		if err != nil {
+			fmt.Println("Воркер отвалился при ожидании ответа. Возвращаем задачу...")
+			retry <- task
+			return
+		}
+		results <- res
 	}
+
 }
 
 func getTask(jobs, retry chan protocol.Task) protocol.Task {
@@ -71,19 +90,19 @@ func getTask(jobs, retry chan protocol.Task) protocol.Task {
 	}
 
 }
-func CuttingAndDistribution(jobs chan protocol.Task) {
-	filePath := "images/input.png"
+func CuttingAndDistribution(jobs chan protocol.Task) (int, *image.RGBA) {
+	filePath := "images/input.jpg"
 	file, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Ошибка: не удалось найти файл по пути %s: %v\n", filePath, err)
-		return
+		return 0, nil
 	}
 	defer file.Close()
 
 	img, format, err := image.Decode(file)
 	if err != nil {
 		fmt.Printf("Ошибка декодирования: %v\n", err)
-		return
+		return 0, nil
 	}
 	fmt.Printf("Файл открыт. Формат: %s, Размер: %dx%d\n", format, img.Bounds().Dx(), img.Bounds().Dy())
 	bounds := img.Bounds()
@@ -93,6 +112,8 @@ func CuttingAndDistribution(jobs chan protocol.Task) {
 	rowsPerTask := RowsPerTask
 	stride := rgbaImg.Stride
 	bytesPerTask := rowsPerTask * stride
+
+	totalTasks := 0
 
 	for i := 0; i < bounds.Dy(); i += rowsPerTask {
 		start := i * stride
@@ -108,5 +129,40 @@ func CuttingAndDistribution(jobs chan protocol.Task) {
 		}
 
 		jobs <- task
+		totalTasks++
+	}
+
+	return totalTasks, rgbaImg
+}
+
+func ImageCollector(results chan protocol.Result, rgbaImg *image.RGBA, totalTasks int) {
+	completed := 0
+	for res := range results {
+		offset := res.Bounds.Min.Y * rgbaImg.Stride
+		copy(rgbaImg.Pix[offset:], res.Payload)
+
+		completed++
+		fmt.Printf("Получен фрагмент %d. Прогресс: %d/%d\n", res.ID, completed, totalTasks)
+
+		if completed == totalTasks {
+			fmt.Println("Все части получены. Формируем файл...")
+
+			saveImage(rgbaImg)
+			fmt.Println("Работа завершена успешно!")
+			os.Exit(0)
+		}
+	}
+}
+
+func saveImage(img *image.RGBA) {
+	f, err := os.Create("images/output.png")
+	if err != nil {
+		fmt.Println("Ошибка при создании файла:", err)
+		return
+	}
+	defer f.Close()
+
+	if err := png.Encode(f, img); err != nil {
+		fmt.Println("Ошибка сохранения PNG:", err)
 	}
 }
